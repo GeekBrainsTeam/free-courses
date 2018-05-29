1bdb5295dfe1718682d4c9db7d76451d8212e7c1
Stanislav Bychkovski <staniby@gmail.com>
2018-05-29 11:40:35 +0300

    dec

--- task1.js
+++ task1.js
@@ -1,3 +1,11 @@
-/* Task 1 */
-
-/* Put your code here */
+function parseNumber() {
+    var number = process.argv[2];
+    var numeralSystem = process.argv[3];
+    var resultNumeralSystem = process.argv[4];
+    var res = 0;
+    if (+resultNumeralSystem === 10) {
+        var res = parseInt(number, +numeralSystem);
+    }
+    return process.stdout.write(res + '\n');
+}
+parseNumber();
