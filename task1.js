f30e3d2e113c898d1efc231c7433172282024a5b
Stanislav Bychkovski <staniby@gmail.com>
2018-05-30 20:35:15 +0300

    fix uotput

--- task1.js
+++ task1.js
@@ -1,15 +1,13 @@
-function parseNumber() {
-    var number = process.argv[1];
-    var numeralSystem = process.argv[2];
-    var resultNumeralSystem = process.argv[3];
-    var res = 0;
-    if (+resultNumeralSystem === 10) {
-        res = parseInt(number, +numeralSystem);
-    } else if (+numeralSystem === 10) {
-        res = Number(number).toString(+resultNumeralSystem);
-    } else {
-        res = Number(+parseInt(number, +numeralSystem)).toString(+resultNumeralSystem);
-    }
-    return process.stdout.write(res + '\n');
+var number = process.argv[2];
+var numeralSystem = process.argv[3];
+var resultNumeralSystem = process.argv[4];
+var res;
+if (+resultNumeralSystem === 10) {
+    res = parseInt(number, +numeralSystem);
+} else if (+numeralSystem === 10) {
+    res = Number(number).toString(+resultNumeralSystem);
+} else {
+    res = Number(+parseInt(number, +numeralSystem)).toString(+resultNumeralSystem);
 }
-parseNumber();
+res=res.toString();
+process.stdout.write(res);
