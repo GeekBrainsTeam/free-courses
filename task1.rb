d4845d1d60deff9f6cefed8798b9e27ec9ff2c76
kengurenokRu <39694111+kengurenokRu@users.noreply.github.com>
2018-06-08 01:25:21 +0300

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -2,9 +2,9 @@
 
 # Put your code here
 if (Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])==0) 
-puts -ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])
+puts (-ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])).round(2)
 elsif (Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])>0)
 if (-ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])>-ARGV[1]-Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])
-puts -ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1]), -ARGV[1]-Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])
-else puts -ARGV[1]-Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1]), -ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])
+puts (-ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])).round(2), (-ARGV[1]-Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])).round(2)
+else puts (-ARGV[1]-Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])).round(2), (-ARGV[1]+Math.sqrt(ARGV[1]**2-4*ARGV[0]*ARGV[1])).round(2)
 else puts "NO ROOTS"
